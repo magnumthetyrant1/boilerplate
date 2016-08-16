6a6682ee5c61e039e694fe0ec0054b372eb1c813
Max-Andre Zils <mzils@dvt.co.za>
2016-08-16 10:45:30 +0200

    removed gulpconfig folder

--- gulpconfig/gulpconcat.js
+++
@@ -1,9 +0,0 @@
-var concat = require('gulp-concat');
-
-module.exports = function (gulp) {
-  gulp.task('scripts', function() {
-  return gulp.src('./public/javascript/*.js')
-    .pipe(concat('all.js'))
-    .pipe(gulp.dest('./public/dist/'));
-  });
-};
